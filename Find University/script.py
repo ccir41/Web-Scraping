af613ae67cc748ce2bc0cd68ea08a2ea89f109f4
ccir41 <shishirsubedi41@gmail.com>
2021-09-03 20:42:15 +0545

    fixed issue with scraping tuition fees

--- Find University/script.py
+++ Find University/script.py
@@ -24,64 +24,41 @@ class FindUniversity:
     def __init__(self):
         self.driver = webdriver.Chrome(executable_path="./chromedriver_linux64/chromedriver", options=chrome_options)
         self.url = "https://www.finduniversity.ph/search.aspx?sch=1&region=national-capital-region"
-        self.csv_headers = []
-        self.append_header = False
+        self.csv_headers = ["name", "location", "application_url", "Bachelor's", "Master's", "Doctorate", "Certificate", "review", "reviewer"]
     
     def extract_data(self):
         self.driver.get(self.url)
-        time.sleep(10)
+        time.sleep(5)
         try:
             dissmiss_button = self.driver.find_element_by_id('dismiss-button')
         except:
             dissmiss_button = None
         if dissmiss_button:
             dissmiss_button.submit()
-        time.sleep(10)
+        time.sleep(5)
         detaildiv = self.driver.find_elements_by_xpath("//div[@class='shell']//div[@class='section__group-main']//div[@class='search-result']//div[@class='box-inner-top']")
         for dv in detaildiv:
-            try:
-                name = dv.find_element_by_tag_name('h3').text
-            except:
-                name = None
-            try:
-                location = dv.find_element_by_xpath("//ul[@class='details']//li[@class='location']").text
-            except:
-                location = None
-            try:
-                application_url = dv.find_element_by_xpath("//ul[@class='details']//li[@class='application']//a").get_attribute('href')
-            except:
-                application_url = None
-            tuitions = dv.find_elements_by_xpath("//ul[@class='details']//li[@class='tuition']")
-            try:
-                bachelor_tuition = tuitions[0].text.strip()
-            except:
-                bachelor_tuition = None
-            try:
-                masters_tuition = tuitions[1].text.strip()
-            except:
-                masters_tuition = None
-            try:
-                doctorate_tuition = tuitions[2].text.strip()
-            except:
-                doctorate_tuition = None
-            try:
-                review = dv.find_element_by_xpath("//div[@class='search-item__review']//p").text.strip()
-            except:
-                review = None
-            try:
-                reviewer = dv.find_element_by_xpath("//div[@class='search-item__review']//p[@class='reviewer']").text.strip()
-            except:
-                reviewer = None
-            data = {
-                "name": name,
-                "location": location,
-                "bachelor_tuition": bachelor_tuition,
-                "masters_tuition": masters_tuition,
-                "doctorate_tuition": doctorate_tuition,
-                "application_url": application_url,
-                "review": review,
-                "reviewer": reviewer
-            }
+            data = {}
+            name = dv.find_element_by_tag_name('h3').text.strip()
+            data["name"] = name
+            details = dv.find_elements_by_tag_name('li')
+            for detail in details:
+                class_attr = detail.get_attribute('class').strip()
+                if class_attr == "location":
+                    data["location"] = detail.text.strip()
+                elif class_attr == "application":
+                    data["application_url"] = detail.find_element_by_tag_name('a').get_attribute('href')
+                elif class_attr == "tuition":
+                    degreespan = detail.find_elements_by_tag_name('span')
+                    try:
+                        degree = degreespan[0].text.strip()
+                        data[f"{degree}"] = degreespan[1].text.strip()
+                    except:
+                        pass
+            #rev = dv.find_elements_by_class_name("search-item__review")
+            #reviews = dv.find_elements_by_tag_name('p')
+            #data["review"] = reviews[0].text.strip()
+            #data["reviewer"] = reviews[1].text.strip()
             self.write_csv(data)
         try:
             next = self.driver.find_element_by_id('cphBody_cphMain_ucBusinessList_ucPagerBottom_hlNext')
@@ -93,14 +70,8 @@ class FindUniversity:
         sys.exit()
     
     def write_csv(self, data):
-        if not self.csv_headers:
-            for key, value in data.items():
-                self.csv_headers.append(key)
         with open('university.csv', 'a+', encoding='utf8', newline='') as output_file:
             writer = csv.DictWriter(output_file, fieldnames=self.csv_headers)
-            if not self.append_header:
-                writer.writeheader()
-                self.append_header = True
             writer.writerow(data)
 
 if __name__ == "__main__":
